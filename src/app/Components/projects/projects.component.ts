ce27195635c8c1a8e8e5742427fcbd9e15504967
Balazs Ferencz <ferenczbalazs85@gmail.com>
2023-05-10 19:39:32 +0100

    project cards are getting data from the api

--- src/app/Components/projects/projects.component.ts
+++ src/app/Components/projects/projects.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from 'src/app/Services/projects.service';
+
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  projectList: any;
+  projectList$: any = [];
 
-  
+  constructor(private ProjectsService: ProjectsService ) { }
+
+  // ngOnInit(): void {
+  //   this.ProjectsService.getProjects().subscribe((data: any) => {
+  //     this.projectList = data;
+  //     console.log(this.projectList);
+  //   })
+  // }
+
+
+  ngOnInit(): void {
+    this.projectList$ = this.ProjectsService.getProjects();
+    console.log(this.projectList$);
+  }
 }
